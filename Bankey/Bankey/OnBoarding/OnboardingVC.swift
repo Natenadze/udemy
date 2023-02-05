d12a86a0f74ad6ff7e302ca6f2533657bdd95bfc
davit <davit.natenadze@gmail.com>
2023-02-05 21:42:31 +0100

    refactor: create generic onboarding VC
    This commit adds a generic onboarding VC that can be reused across multiple onboarding screens.

--- Bankey/Bankey/OnBoarding/OnboardingVC.swift
+++ Bankey/Bankey/OnBoarding/OnboardingVC.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class OnboardingVC: UIViewController {
     
+    var heroImageName: String
+    var titleText: String
+    
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
@@ -18,11 +21,24 @@ class OnboardingVC: UIViewController {
         style()
         layout()
     }
+    
+    init(heroImageName: String, titleText: String){
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension OnboardingVC {
     
     func style() {
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -30,7 +46,7 @@ extension OnboardingVC {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -38,7 +54,7 @@ extension OnboardingVC {
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use and has a brand new look and feel, that will make you feel, like you are back in 1989."
+        label.text = titleText
       
     }
     
